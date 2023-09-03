9726e394f51a306c6326eaf96125ae495fe2c363
sani4618 <sanijoseph2018@gmail.com>
2023-09-03 20:13:31 +0530

    contact part is updated

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <p><b><i><center><h2>Get In Touch</h2></center></i></b></p>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" name='firstName' className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="email" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-control">Phone Number</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Message</label>
+                            <textarea name="" id="" cols="30" rows="5" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-danger">Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
